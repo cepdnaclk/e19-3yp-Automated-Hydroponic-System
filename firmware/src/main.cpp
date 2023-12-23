8d711f6cff30222d32b60c5ef8701c19f137194f
Wishula GitHub <e19166@eng.pdn.ac.lk>
2023-12-23 16:38:22 +0530

    firmware to send ph value to a aws topic added

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,22 +1,39 @@
+// Libraries
 #include <Arduino.h>
 #include "utils.h"
 #include <PubSubClient.h>
+#include "ph_meter.h"
  
+// initializing pins for the relay boards controlling the pumps
+const int waterPumpPin = 23;
+const int nutrientPumpPin = 22;
+const int pHDownPumpPin = 21;
+const int pHUpPumpPin = 19;
 
+// Initializing pins for EC sensor
+const int ecSensorPin = 34;
+const int ecSensorPowerPin = 32;
+const int ecSensorGroundPin = 33;
 
+// Declaring variables for EC sensor
+int R1 = 1000;  // 
+int Ra = 25;  // Resistance of powering Pins
 
+float EC = 0;  // Declaring variable for EC
 
 void setup() {
     Serial.begin(115200);
-    connectAWS();
+    connectAWS(PH_SENSOR_TOPIC);
 }
 
 void loop() {
-  int metricsValue = random(1, 100);
-  Serial.print(F(" metrics: "));
-  Serial.print(metricsValue);
+  checkPhValue();
+  Serial.print(F(" ph: "));
+  
+  Serial.print(phValue);
 
-  publishMessage(metricsValue);
+  
+  publishMessage(phValue, PH_SENSOR_TOPIC, "ph");
   client.loop();
-  delay(1000);
+  delay(2000);
 }
