995def6358013a43c39e6a9e8af669e94792066a
Danujan <sivasundaralingamdanujan@gmail.com>
2023-12-26 00:58:04 +0530

    other classes and configuration files

---
+++ Web Application/AutomatedHydroponicsSystem/AutomatedHydroponicsSystem/src/test/java/com/group15/AutomatedHydroponicsSystem/AutomatedHydroponicsSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group15.AutomatedHydroponicsSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomatedHydroponicsSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
