98dda44beeb293fff6a5d52c0da5f5f947eca273
Wishula GitHub <e19166@eng.pdn.ac.lk>
2023-12-25 23:23:25 +0530

    firmware to activate ph and nutrient pumps added

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -6,13 +6,45 @@
 #include "ph_meter.h"
 #include "ec_meter.h"
 #include "float_sensor.h"
+#include "pumps.h"
 
+const int phAdd = 32;  // declaring increasing ph pump
+const int phMinus = 31;  // declaring decreasing ph pump
+const int nutrientAdd = 30;  // declaring increasing nutrient pump
+
+void activatePumps() {
+    if (tdsValue <= 700 && phValue >= 6.6) {
+        digitalWrite(phMinus, HIGH);
+        digitalWrite(nutrientAdd, HIGH);
+        delay(2000);
+    } else if (tdsValue <= 700 && phValue <= 4.5) {
+        digitalWrite(phAdd, HIGH);
+        digitalWrite(nutrientAdd, HIGH);
+        delay(2000);
+    } else if (phValue >= 6.6) {
+        digitalWrite(phMinus, HIGH);
+        delay(2000);
+    } else if (phValue <= 4.5) {
+        digitalWrite(phAdd, HIGH);
+        delay(2000);
+    } else if (tdsValue <= 700) {
+        digitalWrite(nutrientAdd, HIGH);
+        delay(2000);
+    } else {
+        digitalWrite(phAdd, LOW);
+        digitalWrite(phMinus, LOW);
+        digitalWrite(nutrientAdd, LOW);
+    }
+}
 
 void setup() {
     Serial.begin(115200);
     pinMode(TdsSensorPin,INPUT);
     pinMode(phSensorPin,INPUT);
     pinMode(floatSensorPin,INPUT_PULLUP);
+    pinMode(phAdd,OUTPUT);
+    pinMode(phMinus,OUTPUT);
+    pinMode(nutrientAdd,OUTPUT);
     connectAWS(PH_SENSOR_TOPIC);
 }
 
@@ -27,6 +59,7 @@ void loop() {
   publishMessage(tdsValue, TDS_SENSOR_TOPIC, "tds");
   publishMessage(floatSensorValue, FLOAT_SENSOR_TOPIC, "float");
   client.loop();
+  activatePumps();
   delay(2000);
 }
 
