88d1eccf7ab2045e982dd87ccd1d224f63aede8b
Nithusikan01 <e19266@eng.pdn.ac.lk>
2023-12-26 17:23:18 +0530

    main app updated

--- Web Application/web_application_frontend/src/App.js
+++ Web Application/web_application_frontend/src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
-import NavBar from "./components/Navbar";
-import Home from "../src/components/Users Home/Home_User_New"
+import Home from "./pages/Users Home/Home_User_New"
+import PlantDetails from "./pages/Plant Details/Plant_Details"
+
 
 
 function App() {
   return (
     <>
     <div>
-      <NavBar/>
-      <Home/>
+      {/*<Home/>*/}
+      <PlantDetails/>
     </div>
     </>
   );
