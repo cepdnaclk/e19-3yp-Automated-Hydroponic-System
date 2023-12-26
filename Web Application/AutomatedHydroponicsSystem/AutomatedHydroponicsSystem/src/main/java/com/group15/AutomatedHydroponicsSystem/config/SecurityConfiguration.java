d19ee6a98499ac437935bae81bf9e68a92b6bd36
Danujan <sivasundaralingamdanujan@gmail.com>
2023-12-26 11:42:36 +0530

    LogoutService files added

--- Web Application/AutomatedHydroponicsSystem/AutomatedHydroponicsSystem/src/main/java/com/group15/AutomatedHydroponicsSystem/config/SecurityConfiguration.java
+++ Web Application/AutomatedHydroponicsSystem/AutomatedHydroponicsSystem/src/main/java/com/group15/AutomatedHydroponicsSystem/config/SecurityConfiguration.java
@@ -8,8 +8,10 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -18,6 +20,7 @@ public class SecurityConfiguration {
 
     private final JwtAuthenticationFilter jwtAuthFilter;
     private final AuthenticationProvider authenticationProvider;
+    private final LogoutHandler logoutHandler;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
 
@@ -35,7 +38,13 @@ public class SecurityConfiguration {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .logout()
+                .logoutUrl("/api/v1/auth/logout")
+                .addLogoutHandler(logoutHandler)
+                .logoutSuccessHandler((request,response,authentication) -> SecurityContextHolder.clearContext())
+            ;
+
                 
 
         return http.build();
