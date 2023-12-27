43157941fd9203fbdc58d8a9b14a5ae70d8bfbda
Danujan <sivasundaralingamdanujan@gmail.com>
2023-12-27 08:05:43 +0530

    New roles User,Admin,Manager added

--- Web Application/AutomatedHydroponicsSystem/AutomatedHydroponicsSystem/src/main/java/com/group15/AutomatedHydroponicsSystem/User/Role.java
+++ Web Application/AutomatedHydroponicsSystem/AutomatedHydroponicsSystem/src/main/java/com/group15/AutomatedHydroponicsSystem/User/Role.java
@@ -1,8 +1,47 @@
 package com.group15.AutomatedHydroponicsSystem.User;
 
+import lombok.Getter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+import static com.group15.AutomatedHydroponicsSystem.User.Permission.*;
+
 public enum Role {
 
-    USER,
-    ADMIN,
-    MANAGER
+    USER(Collections.emptySet()),
+    ADMIN(
+            Set.of(
+                    ADMIN_READ,
+                    ADMIN_UPDATE,
+                    ADMIN_DELETE,
+                    ADMIN_CREATE,
+                    MANAGER_READ,
+                    MANAGER_UPDATE,
+                    MANAGER_CREATE,
+                    MANAGER_DELETE
+            )
+    ),
+    MANAGER(
+            Set.of(
+                    MANAGER_READ,
+                    MANAGER_UPDATE,
+                    MANAGER_CREATE,
+                    MANAGER_DELETE
+            )
+    );
+
+    @Getter
+    private final Set<Permission> permissions;
+
+    public List<SimpleGrantedAuthority> getAuthorities(){
+        var authorities = getPermissions()
+                .stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.name()))
+                .toList();
+        authorities.add(new SimpleGrantedAuthority("Role_" + this.name()));
+        return authorities;
+    }
 }
