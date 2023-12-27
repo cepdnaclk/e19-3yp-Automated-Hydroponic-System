35e2bbc2bfc82af2a5f3c82d942b216f84201709
Sandamini Madhurasinghe <e19224@eng.pdn.ac.lk>
2023-12-28 00:31:11 +0530

    Profile page

--- Mobile Application/flutter_app/lib/profile.dart
+++ Mobile Application/flutter_app/lib/profile.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 class Profile extends StatefulWidget {
@@ -10,6 +12,109 @@ class Profile extends StatefulWidget {
 class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
-    return Container(child: Text("Profile"),);
+    return Scaffold(
+      body: Column(
+        children: [
+          Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 10.0, right: 20.0),
+                  child: IconButton(
+                    onPressed: () {
+                      // Handle onPressed action
+                    },
+                    icon: Icon(
+                      Icons.edit,
+                      size: 16,
+                      color: Color(0xFF0D7817),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          Container(
+                margin: EdgeInsets.only(top: 20.0, bottom: 20.0),
+                height: 150,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: AssetImage("assets/Profile.jpg"),
+                  ),
+                ),
+              ),
+          Text(
+            "Yashoda De Silva",
+            style: TextStyle(
+              fontFamily: 'Poppins',
+              fontSize: 15,
+              color: Color(0xFF0D7817),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 20.0),
+            child: Text(
+              "Farmer ID : 12",
+              style: TextStyle(
+                fontFamily: 'Poppins',
+                fontSize: 12,
+                color: Colors.grey,
+              ),
+            ),
+          ),
+
+          Divider(thickness: 0.2,),
+
+          Padding(
+            padding: const EdgeInsets.only(top:20.0,left: 40.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    "Plants owned",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontSize: 16,
+                    color: Colors.black,
+                    fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top:20.0,left: 40.0),
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Image(
+                    //size: 14,
+                    height: 15.0,
+                    image: AssetImage("assets/planticon.png"),
+                    // Additional properties like size, color, etc., can be added here
+                  ),
+                ),
+                Expanded(
+                  flex: 5,
+                  child: Text(
+                    "Aloe Vera",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontSize: 15,
+                    color: Colors.black,
+                    fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
