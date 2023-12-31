aacecaa41f5de2c5bdcf965e97fa0e37765e9d50
Nithusikan01 <e19266@eng.pdn.ac.lk>
2023-12-31 19:42:36 +0530

    plant entity package updated

---
+++ Web Application/AutomatedHydroponicsSystem/AutomatedHydroponicsSystem/src/main/java/com/group15/AutomatedHydroponicsSystem/plants/PlantRepository.java
@@ -0,0 +1,7 @@
+package com.group15.AutomatedHydroponicsSystem.plants;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlantRepository extends JpaRepository<Plant, Integer> {
+
+}
