8f6a6aea97124ac14e559efbd4333233ca98f869
KasuniH <99702168+KasuniH@users.noreply.github.com>
2023-12-31 23:02:39 +0530

    Add files via upload

---
+++ Binding/Help/contactcontroller.java
@@ -0,0 +1,23 @@
+// ContactController.java
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/contacts")
+public class ContactController {
+
+    @Autowired
+    private ContactRepository contactRepository;
+
+    @GetMapping
+    public List<Contact> getAllContacts() {
+        return contactRepository.findAll();
+    }
+
+    @PostMapping
+    public Contact addContact(@RequestBody Contact contact) {
+        return contactRepository.save(contact);
+    }
+}
