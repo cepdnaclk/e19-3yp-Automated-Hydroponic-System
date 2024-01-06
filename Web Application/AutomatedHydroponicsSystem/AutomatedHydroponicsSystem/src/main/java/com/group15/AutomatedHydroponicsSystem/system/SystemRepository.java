1dde1efe57063536fb1b51bf9a39b6a4b4446970
Nithusikan01 <e19266@eng.pdn.ac.lk>
2024-01-06 21:45:11 +0530

    system repository updated

--- Web Application/AutomatedHydroponicsSystem/AutomatedHydroponicsSystem/src/main/java/com/group15/AutomatedHydroponicsSystem/system/SystemRepository.java
+++ Web Application/AutomatedHydroponicsSystem/AutomatedHydroponicsSystem/src/main/java/com/group15/AutomatedHydroponicsSystem/system/SystemRepository.java
@@ -1,6 +1,8 @@
 package com.group15.AutomatedHydroponicsSystem.system;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SystemRepository extends JpaRepository<System,Integer> {
 }
