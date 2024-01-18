c428c9a1187d8390c82c2b62f7db2142aa1ae8d0
Danujan <sivasundaralingamdanujan@gmail.com>
2024-01-19 00:59:22 +0530

    Default admin and manager entity creation while starting the application is disabled

--- Web Application/AutomatedHydroponicsSystem/AutomatedHydroponicsSystem/src/main/java/com/group15/AutomatedHydroponicsSystem/AutomatedHydroponicsSystemApplication.java
+++ Web Application/AutomatedHydroponicsSystem/AutomatedHydroponicsSystem/src/main/java/com/group15/AutomatedHydroponicsSystem/AutomatedHydroponicsSystemApplication.java
@@ -3,6 +3,7 @@ package com.group15.AutomatedHydroponicsSystem;
 import com.group15.AutomatedHydroponicsSystem.User.Role;
 import com.group15.AutomatedHydroponicsSystem.auth.AuthenticationService;
 import com.group15.AutomatedHydroponicsSystem.auth.RegisterRequest;
+import com.group15.AutomatedHydroponicsSystem.auth.UserAlreadyExistsException;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,28 +16,36 @@ public class AutomatedHydroponicsSystemApplication {
 		SpringApplication.run(AutomatedHydroponicsSystemApplication.class, args);
 	}
 
-	@Bean
-	public CommandLineRunner commandLineRunner(
-			AuthenticationService service
-	){
-		return args -> {
-			var admin = RegisterRequest.builder()
-					.firstname("Admin")
-					.lastname("Admin")
-					.email("admin@mail.com")
-					.password("password")
-					.role(Role.ADMIN)
-					.build();
-			System.out.println("Admin token: "+ service.register(admin).getAccessToken());
-
-			var manager = RegisterRequest.builder()
-					.firstname("MANAGER")
-					.lastname("MANAGER")
-					.email("manager@mail.com")
-					.password("password")
-					.role(Role.MANAGER)
-					.build();
-			System.out.println("MANAGER token: "+ service.register(manager).getAccessToken());
-		};
-	}
+//	@Bean
+//	public CommandLineRunner commandLineRunner(
+//			AuthenticationService service
+//	){
+//		return args -> {
+//			var admin = RegisterRequest.builder()
+//					.firstname("Admin")
+//					.lastname("Admin")
+//					.email("admin@mail.com")
+//					.password("password")
+//					.role(Role.ADMIN)
+//					.build();
+//            try {
+//                System.out.println("Admin token: "+ service.register(admin).getAccessToken());
+//            } catch (UserAlreadyExistsException e) {
+//                throw new RuntimeException(e);
+//            }
+//
+//            var manager = RegisterRequest.builder()
+//					.firstname("MANAGER")
+//					.lastname("MANAGER")
+//					.email("manager@mail.com")
+//					.password("password")
+//					.role(Role.MANAGER)
+//					.build();
+//            try {
+//                System.out.println("MANAGER token: "+ service.register(manager).getAccessToken());
+//            } catch (UserAlreadyExistsException e) {
+//                throw new RuntimeException(e);
+//            }
+//        };
+//	}
 }
