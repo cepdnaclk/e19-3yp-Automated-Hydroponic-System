3b8cfffc63b833e4def0d827a24de37825d31c43
Nithusikan01 <e19266@eng.pdn.ac.lk>
2024-01-23 01:01:26 +0530

    plant controller test updated

--- Web Application/AutomatedHydroponicsSystem/AutomatedHydroponicsSystem/src/test/java/com/group15/AutomatedHydroponicsSystem/Plant/PlantControllerTest.java
+++ Web Application/AutomatedHydroponicsSystem/AutomatedHydroponicsSystem/src/test/java/com/group15/AutomatedHydroponicsSystem/Plant/PlantControllerTest.java
@@ -1,21 +1,14 @@
 package com.group15.AutomatedHydroponicsSystem.Plant;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.group15.AutomatedHydroponicsSystem.Exception.PlantNotFoundException;
-import com.group15.AutomatedHydroponicsSystem.plants.Plant;
-import com.group15.AutomatedHydroponicsSystem.plants.PlantController;
 
-import com.group15.AutomatedHydroponicsSystem.plants.PlantRepository;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.Arrays;
