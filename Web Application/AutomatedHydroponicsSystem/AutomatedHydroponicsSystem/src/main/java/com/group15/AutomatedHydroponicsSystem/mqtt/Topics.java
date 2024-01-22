6880de460b9b221f2396ef6e4cd7aa414138d358
Nithusikan01 <e19266@eng.pdn.ac.lk>
2024-01-22 20:44:13 +0530

    Topics class file added

---
+++ Web Application/AutomatedHydroponicsSystem/AutomatedHydroponicsSystem/src/main/java/com/group15/AutomatedHydroponicsSystem/mqtt/Topics.java
@@ -0,0 +1,17 @@
+package com.group15.AutomatedHydroponicsSystem.mqtt;
+
+public class Topics {
+    //topics
+    public static final String phsensorTopic = "phsensor";
+    public static final String tdssensorTopic = "tdssensor";
+    public static final String floatsensorTopic = "floatsensor";
+    public static final String phpumpTopic = "phpump";
+    public static final String phlowpumpTopic = "phlowpump";
+    public static final String tdspumpTopic = "tdspump";
+    public static final String phhigherrorTopic = "phhigherror";
+    public static final String phlowerrorTopic = "phlowerror";
+    public static final String tdserrorTopic = "tdserror";
+    public static final String phsensorerrorTopic = "phsensorerror";
+    public static final String tdssensorerrorTopic = "tdssensorerror";
+
+}
