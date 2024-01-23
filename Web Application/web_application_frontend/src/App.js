c4c57212167fd7a898c44221fb6b53205886cdbb
Danujan <sivasundaralingamdanujan@gmail.com>
2024-01-24 00:13:29 +0530

    File Structure Changed

--- Web Application/web_application_frontend/src/App.js
+++ Web Application/web_application_frontend/src/App.js
@@ -10,7 +10,6 @@ function App() {
   return (
     <>
     <div>
-
       <Router basename="/">
         <RoutesConfig/>
       </Router>
--- Web Application/web_application_frontend/src/pages/1_Commonpage/LandingPage/HomePage.js
+++ Web Application/web_application_frontend/src/pages/1_Commonpage/LandingPage/HomePage.js
@@ -1,7 +1,7 @@
 import React from "react";
-import "./LandingPage.css";
-import CommonNavBar from "../../components/Common_NavBar/Common_Navbar";
-import Symbol from "../../Images/symbol.png"
+import "./HomePage.css";
+import CommonNavBar from "../../../components/Common_NavBar/Common_Navbar";
+import Symbol from "../../../Images/symbol.png"
 
 function LandingPage(){
     return(
@@ -10,10 +10,11 @@ function LandingPage(){
         <div className="main-container row">
             <div className="bg-image-container row">
                 <div className="text-container row">
-                    <h1 style={{color:"#0D7817"}}>Want An Automated Hydroponic System?</h1>
+                    <br/>
+                    <h1 style={{color:"#0D7817"}}>Garden of Tomorrow, Today: Immerse Yourself in the Ease of Automated Hydroponics!</h1>
                 </div>
                 <div className="secondary-container ">
-                    <img src={Symbol} alt="Symbol Loading..." className="symbol-image row"/>
+                    <img src={Symbol} alt="Symbol Loading..." className="symbol-image row" width="1400" height="1400"/>
                     <button className="join-us-button row">Join Us</button>
                 </div>
             </div>
