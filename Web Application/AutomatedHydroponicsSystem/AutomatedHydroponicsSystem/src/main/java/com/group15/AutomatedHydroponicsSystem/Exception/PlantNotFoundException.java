2ee4c34ee087c39aa1cab0028ec76c15ef7241b0
Nithusikan01 <115540028+Nithusikan01@users.noreply.github.com>
2024-01-25 11:31:50 -0800

    Minor PlantNotFoundException.java

--- Web Application/AutomatedHydroponicsSystem/AutomatedHydroponicsSystem/src/main/java/com/group15/AutomatedHydroponicsSystem/Exception/PlantNotFoundException.java
+++ Web Application/AutomatedHydroponicsSystem/AutomatedHydroponicsSystem/src/main/java/com/group15/AutomatedHydroponicsSystem/Exception/PlantNotFoundException.java
@@ -2,6 +2,6 @@ package com.group15.AutomatedHydroponicsSystem.Exception;
 
 public class PlantNotFoundException extends RuntimeException{
     public PlantNotFoundException(Integer id) {
-        super("Could not found any plant with the give id: " + id);
+        super("Could not found any plant with the given id: " + id);
     }
 }
