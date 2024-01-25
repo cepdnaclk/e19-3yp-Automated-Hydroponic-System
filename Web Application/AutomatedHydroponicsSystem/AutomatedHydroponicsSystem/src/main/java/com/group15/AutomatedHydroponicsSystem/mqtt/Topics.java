5bd2c123010a8058626dd2aeb0d9b43be5f28289
Nithusikan01 <115540028+Nithusikan01@users.noreply.github.com>
2024-01-25 11:35:07 -0800

    Update Topics.java

--- Web Application/AutomatedHydroponicsSystem/AutomatedHydroponicsSystem/src/main/java/com/group15/AutomatedHydroponicsSystem/mqtt/Topics.java
+++ Web Application/AutomatedHydroponicsSystem/AutomatedHydroponicsSystem/src/main/java/com/group15/AutomatedHydroponicsSystem/mqtt/Topics.java
@@ -1,17 +1,36 @@
 package com.group15.AutomatedHydroponicsSystem.mqtt;
 
 public class Topics {
-    //topics
-    public static final String phsensorTopic = "phsensor";
-    public static final String tdssensorTopic = "tdssensor";
-    public static final String floatsensorTopic = "floatsensor";
-    public static final String phpumpTopic = "phpump";
-    public static final String phlowpumpTopic = "phlowpump";
-    public static final String tdspumpTopic = "tdspump";
-    public static final String phhigherrorTopic = "phhigherror";
-    public static final String phlowerrorTopic = "phlowerror";
-    public static final String tdserrorTopic = "tdserror";
-    public static final String phsensorerrorTopic = "phsensorerror";
-    public static final String tdssensorerrorTopic = "tdssensorerror";
+    //Topics to be subscribed
+    public static final String phsensorTopic = "phsensor";   // key : ph
+    public static final String tdssensorTopic = "tdssensor";  // key : tds
+    public static final String floatsensorTopic = "floatsensor";  // key : float
+
+
+    public static final String phpumpTopic = "phpump";   // key : phhighpumpstate
+    public static final String phlowpumpTopic = "phlowpump";  // key : phlowpumpstate
+    public static final String tdspumpTopic = "tdspump";  // key : tdspumpstate
+
+
+    public static final String phhigherrorTopic = "phhigherror";  // key : phhigherror
+    public static final String phlowerrorTopic = "phlowerror";  // key : phlowerror
+    public static final String tdserrorTopic = "tdserror";  // key : tdserror
+
+
+    public static final String phsensorerrorTopic = "phsensorerror";  // key : phsensorerror
+    public static final String tdssensorerrorTopic = "tdssensorerror";  // key : tdssensorerror
+
+
+    //Topics to be published   common key : message
+    public static final String tdsLow = "tdslow";
+    public static final String tdsHigh = "tdshigh";
+    public static final String phHigh = "phhigh";
+    public static final String phLow = "phlow";
+    public static final String waterPump = "waterpump";
+    public static final String pHLowPumpBack = "phlowpumpBack";
+    public static final String pHHighPumpBack = "phpumpBack";
+    public static final String tdsPumpBack = "tdspumpBack";
+
+
 
 }
